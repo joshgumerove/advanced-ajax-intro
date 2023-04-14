2cedfb6da84d934c2900ed68aee20c059937fe34
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-04-14 11:58:09 -0400

    fetch refactor completed

--- XHR_recap/app.js
+++ XHR_recap/app.js
@@ -32,46 +32,68 @@ let JSONButton = document.getElementById("json");
 let JSONCard = document.getElementById("JSONCard");
 
 JSONButton.addEventListener("click", () => {
-  let xhr = new XMLHttpRequest();
-  let method = "GET";
+  // let xhr = new XMLHttpRequest();
+  // let method = "GET";
   let url = "../data/data.json";
 
-  xhr.open(method, url);
-  xhr.onload = () => {
-    if (xhr.status === 200) {
-      let dataObject = JSON.parse(xhr.responseText);
-      showJSONData(dataObject);
-    }
-  };
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      JSONCard.innerHTML = `<p>${data.book} ${data.verse}</p>`;
+    })
+    .catch((err) => console.log("there is an error: ", err));
 
-  let showJSONData = (data) => {
-    let messageTemplate = `<p>${data.book} ${data.verse}</p>`;
-    JSONCard.innerHTML = messageTemplate;
-  };
-  xhr.send();
+  // xhr.open(method, url);
+  // xhr.onload = () => {
+  //   if (xhr.status === 200) {
+  //     let dataObject = JSON.parse(xhr.responseText);
+  //     showJSONData(dataObject);
+  //   }
+  // };
+
+  // let showJSONData = (data) => {
+  //   let messageTemplate = `<p>${data.book} ${data.verse}</p>`;
+  //   JSONCard.innerHTML = messageTemplate;
+  // };
+  // xhr.send();
 });
 
 let apiButton = document.getElementById("api");
 let apiCard = document.getElementById("ApiCard");
 
 apiButton.addEventListener("click", () => {
-  let xhr = new XMLHttpRequest();
-  let method = "GET";
+  // let xhr = new XMLHttpRequest();
+  // let method = "GET";
   let url = "https://bible-api.com/john 3:16";
 
-  xhr.open(method, url);
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      apiCard.innerHTML = `<p>${data.reference} ${data.text}</p>`;
+    })
+    .catch((err) => console.log("there is an error: ", err));
 
-  xhr.onload = () => {
-    if (xhr.status === 200) {
-      let apiData = JSON.parse(xhr.responseText);
-      showApi(apiData);
-    }
-  };
+  // xhr.open(method, url);
 
-  let showApi = (data) => {
-    let messageTemplate = `<p>${data.reference} ${data.text}</p>`;
-    apiCard.innerHTML = messageTemplate;
-  };
+  // xhr.onload = () => {
+  //   if (xhr.status === 200) {
+  //     let apiData = JSON.parse(xhr.responseText);
+  //     showApi(apiData);
+  //   }
+  // };
 
-  xhr.send();
+  // let showApi = (data) => {
+  //   let messageTemplate = `<p>${data.reference} ${data.text}</p>`;
+  //   apiCard.innerHTML = messageTemplate;
+  // };
+
+  // xhr.send();
 });
