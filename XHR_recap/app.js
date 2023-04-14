10393a2d920ba0311dcd55a6445ce91e38244154
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-04-14 02:19:47 -0400

    fetch refactor

--- XHR_recap/app.js
+++ XHR_recap/app.js
@@ -2,19 +2,24 @@ let textButton = document.getElementById("text");
 let textCard = document.getElementById("textCard");
 
 textButton.addEventListener("click", () => {
-  let xhr = new XMLHttpRequest();
+  // let xhr = new XMLHttpRequest();
   let method = "GET";
   let url = "../data/text.txt";
 
-  xhr.open(method, url);
-  xhr.send();
+  fetch(url)
+    .then((response) => response.text())
+    .then((result) => showTextData(result))
+    .catch((err) => console.log("error: ", err));
 
-  xhr.onload = () => {
-    if (xhr.status == 200) {
-      let data = xhr.responseText;
-      showTextData(data);
-    }
-  };
+  // xhr.open(method, url);
+  // xhr.send();
+
+  // xhr.onload = () => {
+  //   if (xhr.status == 200) {
+  //     let data = xhr.responseText;
+  //     showTextData(data);
+  //   }
+  // };
 
   let showTextData = (data) => {
     let messageTemplate = `<p>${data}</p>`;
