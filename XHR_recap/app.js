130d1bc3e67760f18e73533c493f100b7009c2ee
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-04-13 21:43:09 -0400

    get tex data

---
+++ XHR_recap/app.js
@@ -0,0 +1,18 @@
+let textButton = document.getElementById("text");
+let textCard = document.getElementById("textCard");
+
+textButton.addEventListener("click", () => {
+  let xhr = new XMLHttpRequest();
+  let method = "GET";
+  let url = "../data/text.txt";
+
+  xhr.open(method, url);
+  xhr.send();
+
+  xhr.onload = () => {
+    if (xhr.status == 200) {
+      let data = xhr.responseText;
+      textCard.innerHTML = data;
+    }
+  };
+});
